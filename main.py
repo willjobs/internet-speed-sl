fa6293f2fc76b7454b1ecd775fb705849cd0bac8
Will Jobs <7775919+willjobs@users.noreply.github.com>
2024-07-19 21:49:33 -0400

    Updated to include summary stats, zoom to past week, adjust whitespace, always show modebar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
@@ -47,8 +49,51 @@ def parse_data(content):
     return pd.DataFrame(data)
 
 
+def get_summary_stats(df: pd.DataFrame) -> str:
+    df = df.set_index('timestamp')
+    current_time = df.index.max()
+
+    def calc_stats(start_time):
+        data = df.loc[start_time:, 'download']
+        return {
+            'min': data.min(),
+            'median': data.median(),
+            'max': data.max()
+        }
+
+    hour_stats = calc_stats(current_time - pd.Timedelta(hours=1))
+    day_stats = calc_stats(current_time - pd.Timedelta(days=1))
+    week_stats = calc_stats(current_time - pd.Timedelta(weeks=1))
+
+    return f"""
+    <table>
+        <tr><th>Timeframe</th><th>median</th><th>range</th></tr>
+        <tr><td>Last 1 hour</td><td>{hour_stats['median']:.1f} Mbps</td><td>{hour_stats['min']:.1f} &ndash; {hour_stats['max']:.1f} Mbps</td></tr>
+        <tr><td>Last 1 day</td><td>{day_stats['median']:.1f} Mbps</td><td>{day_stats['min']:.1f} &ndash; {day_stats['max']:.1f} Mbps</td></tr>
+        <tr><td>Last 1 day</td><td>{week_stats['median']:.1f} Mbps</td><td>{week_stats['min']:.1f} &ndash; {week_stats['max']:.1f} Mbps</td></tr>
+    </table>
+    """
+
+
 def main():
-    st.set_page_config(layout="wide")  # Set the page to wide mode
+    st.set_page_config(layout="wide")
+    st.markdown("""
+        <style>
+            #MainMenu {visibility: hidden;}
+            .stPlotlyChart {height: 75vh !important;}
+            footer {visibility: hidden;}
+            .block-container {
+                padding-top: 3rem;
+                padding-bottom: 0rem;
+                padding-left: 5rem;
+                padding-right: 5rem;
+            }
+            .modebar-container {
+                margin-top: 20px !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
     st.title("Chappy Internet Speed")
 
     file_content = read_file_from_dropbox('/internet_speed/speed_tests.txt')
@@ -56,6 +101,8 @@ def main():
         return
 
     df = parse_data(file_content)
+    stats_str = f"{get_summary_stats(df)}\n<p>Click the 'Autoscale' button to zoom out to entire date range</p>"
+    st.markdown(stats_str, unsafe_allow_html=True)
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df['timestamp'], y=df['download'], mode='lines', name='Download Speed'))
@@ -64,16 +111,28 @@ def main():
     fig.update_layout(
         yaxis_title='Speed (Mbps)',
         hovermode='closest',
-        height=600,  # Increase the height of the plot
-        margin=dict(l=50, r=50, t=50, b=50)  # Adjust margins
+        autosize=False,
+        margin=dict(l=10, r=10, t=0, b=10),
+        legend=dict(
+            x=0.2,
+            y=0.9,
+            orientation="h",
+            xanchor="right",
+            yanchor="auto",
+        ),
     )
 
     fig.update_traces(
         hovertemplate='<b>Timestamp</b>: %{x}<br><b>Speed</b>: %{y:.2f} Mbps'
     )
 
+    # set the initial zoom to be the past week of data
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=7)
+    fig.update_xaxes(type="date", range=[start_date, datetime.today().strftime("%Y-%m-%d")])
+
     # Display the plot
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True, config={"displayModeBar": True})
 
 if __name__ == "__main__":
     main()
